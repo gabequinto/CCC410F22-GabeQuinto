adbb40760490e30750e988c5fc1a168081d1f8b2
gabequinto <78443183+gabequinto@users.noreply.github.com>
2022-10-16 12:22:06 -0400

    Update main.go

--- docs/topics/golang/main.go
+++ docs/topics/golang/main.go
@@ -3,80 +3,42 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"log"
-	"strings"
-
 	"golang.org/x/crypto/ssh"
 )
 
-type Connection struct {
-	*ssh.Client
-	password string
-}
-//connecting to remote machine without host keys
-func Connect(addr, user, password string) (*Connection, error) {
-	sshConfig := &ssh.ClientConfig{
-		User: user,
+var username = "gabe"
+var password = "password123"
+var host = "192.168.229.138:22"
+var command = "echo hi"
+
+func main() {
+	//authenitcation
+	config := &ssh.ClientConfig{
+		User: username,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-
-	conn, err := ssh.Dial("tcp", addr, sshConfig)
+	//connect to remote system
+	client, err := ssh.Dial("tcp", host, config)
 	if err != nil {
-		return nil, err
+		panic("Error dialing server. " + err.Error())
 	}
-
-	return &Connection{conn, password}, nil
-
-}
-
-// run commands as sudo user
-func (conn *Connection) SendCommands(cmds string) ([]byte, error) {
-	session, err := conn.NewSession()
+	//create session
+	session, err := client.NewSession()
 	if err != nil {
-		log.Fatal(err)
+		panic("Failed creating session" + err.Error())
 	}
 	defer session.Close()
 
-	stdoutB := new(bytes.Buffer)
-	session.Stdout = stdoutB
-	in, _ := session.StdinPipe()
+	var b bytes.Buffer
+	//session output
+	session.Stdout = &b
+	//run command on remote system
 
-	go func(in io.Writer, output *bytes.Buffer) {
-		for {
-			if strings.Contains(string(output.Bytes()), "[sudo] password for ") {
-				_, err = in.Write([]byte(conn.password + "\n"))
-				if err != nil {
-					break
-				}
-				fmt.Println("put the password ---  end .")
-				break
-			}
-		}
-	}(in, stdoutB)
-
-	err = session.Run(cmds)
-	if err != nil {
-		return []byte{}, err
+	if err := session.Run(command); err != nil {
+		panic("Command Failed. " + err.Error())
 	}
-	return stdoutB.Bytes(), nil
+	fmt.Println(b.String())
 }
-
-//define computer, user, and password
-func main() {
-
-	conn, err := Connect("192.168.229.135:22", "gabe", "password123")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// command to execute
-	output, err := conn.SendCommands("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	// print output of remote command
-	fmt.Println(string(output))
-	
